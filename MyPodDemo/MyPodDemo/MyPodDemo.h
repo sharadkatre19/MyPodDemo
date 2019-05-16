85e92578f03d465657a87b2733710743bf1841b6
Sharad Katre <sharadkatre@mipl.local>
2019-05-16 11:39:52 +0530

    initial commit

---
+++ MyPodDemo/MyPodDemo/MyPodDemo.h
@@ -0,0 +1,19 @@
+//
+//  MyPodDemo.h
+//  MyPodDemo
+//
+//  Created by Sharad Katre on 16/05/19.
+//  Copyright © 2019 Sharad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyPodDemo.
+FOUNDATION_EXPORT double MyPodDemoVersionNumber;
+
+//! Project version string for MyPodDemo.
+FOUNDATION_EXPORT const unsigned char MyPodDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyPodDemo/PublicHeader.h>
+
+
